b2a2636eb7c5d65d76b755d2d61da8006a0f4deb
Josué Muñoz S <jmunozs@ucatolica.ac.cr>
2016-06-27 01:45:10 -0600

    adelanto
    terminado
    un 30% de la parte escrita.
    un 40% de la parte programada.

--- juegoahorcado/src/juegoahorcado/Juegoahorcado.java
+++ juegoahorcado/src/juegoahorcado/Juegoahorcado.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package juegoahorcado;
+//importe de la clase scanner, para agarrar letras ingresadas por usuario
 import java.util.Scanner;
+//importe de la clase random que se encarga de escoger una palabra alazar
+import java.util.Random;
 /**
  *
  * @author Josue Muñoz S.
@@ -14,9 +17,43 @@ public class Juegoahorcado {
 
     public static void main(String[] args) {
        
-        Scanner scanner = new Scanner(System.in);
-        String [] listaPalabras = {"HEREDIANO","herediano","SAPRISSA","saprissa","ALAJUELA","alajuela","CARTAGO","cartago","SANCARLOS","sancarlos","BELEN","belen","LIMON","limon"};
-        System.out.println("Hola! A que no adivinas la palabra que tengo en mente ¿?... te atreves a jugar?");
+        Juegoahorcado juegoahrocado = new Juegoahorcado ();
     }
-    
+    public Juegoahorcado(){
+    // metodo scaner que agarra lo ingresado por el usuario
+    Scanner scanner = new Scanner(System.in);
+    //metodo string donde almacena una lista de palabras que creamos y que se utilizaran en el juego.    
+    String [] todaslasPalabras = {"HEREDIANO","herediano","SAPRISSA","saprissa","ALAJUELA","alajuela","CARTAGO","cartago","SANCARLOS","sancarlos","BELEN","belen","LIMON","limon"};
+    // mensaje de salida, que se mostrara en la consola 
+    System.out.println("Hola! A que no adivinas la palabra que tengo en mente ¿?... te atreves a jugar?");
+
+        int numeroRandom = 1 + new Random().nextInt(todaslasPalabras.length);
+        String palabraElegida = todaslasPalabras[numeroRandom];
+        System.out.println("de acuerdo la palabra consta de " + palabraElegida.length() + " letras...");
+        System.out.println("SUERTE!!!");
+        String[] palabra = new String[palabraElegida.length()];
+        imprimirPalabra(palabra);
+        int vidas = 11;
+
+        while (true) {
+            System.out.println(" ");
+            System.out.println("Ok es facil, solo digita una letra, la que quieras ➜ [(  )]");
+            String letraElegidaPorUsuario = scanner.nextLine();
+            String[] letras = palabraElegida.split("");
+            int contador = 0;
+
+            boolean adivinoAlguna = false;
+
+            
+            for (String letra : letras) {
+
+                if (letraElegidaPorUsuario.equals(letra)) {
+                    palabra[contador] = letraElegidaPorUsuario;
+                    adivinoAlguna = true;
+                }
+
+                contador++;
+            }
+    }
+ }
 }
